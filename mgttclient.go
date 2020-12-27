7ab13c06472abaae8bda4b45e65965e494309e4b
James Alspach <jalspach@gmail.com>
2020-12-26 23:57:46 -0800

    move to hive tests

--- mgttclient.go
+++ mgttclient.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	//import the Paho Go MQTT library
+	"log"
+	"net"
 	"os"
 	"time"
 
@@ -11,6 +13,19 @@ import (
 
 //watch here: http://www.hivemq.com/demos/websocket-client/
 //documentation here: https://pkg.go.dev/github.com/eclipse/paho.mqtt.golang
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
 
 //define a function for the default message handler
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
@@ -21,10 +36,12 @@ var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 func main() {
 	//create a ClientOptions struct setting the broker address, clientid, turn
 	//off trace output and set the default message handler
-	opts := MQTT.NewClientOptions().AddBroker("tcp://broker.hivemq.com:1883")
-	opts.SetClientID("clientId-memememe")
+	opts := MQTT.NewClientOptions().AddBroker("mqtt://localhost:1883")
+	opts.SetClientID("gotest")
+	opts.SetUsername("testclient")
+	opts.SetPassword("1qaz2wsx")
 	opts.SetDefaultPublishHandler(f)
-	opts.SetWill("testtopic/james/lwt", "HCF - Elvis has left the building", 2, false)
+	opts.SetWill("/Device/james/lwt", "HCF - Elvis has left the building", 2, false)
 
 	//create and start a client using the above ClientOptions
 	c := MQTT.NewClient(opts)
@@ -34,28 +51,33 @@ func main() {
 
 	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
 	//at a maximum qos of zero, wait for the receipt to confirm the subscription
-	if token := c.Subscribe("testtopic/james/#", 0, nil); token.Wait() && token.Error() != nil {
+	if token := c.Subscribe("/device/#", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
+	//dt := time.Now()
+	//curr := fmt.Sprintf("Current date and time is: $d", dt.Format("01-02-2006 15:04:05"))
+	//token2 := c.Publish("/testtopic/james/curtime", 1, false, curr)
+	//token2.Wait()
+
+	ipaddr := GetOutboundIP()
+	//ipreport := fmt.Sprintf(ip)
+	token := c.Publish("device/imac", 0, false, ipaddr)
+	token.Wait()
 
 	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
 	//from the server after sending each message
+
 	for i := 0; i < 5; i++ {
 		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish("testtopic/james/3", 0, false, text)
+		token := c.Publish("device/james/3", 0, false, text)
 		token.Wait()
-
-		dt := time.Now()
-		curr := fmt.Sprintf("Current date and time is: $d", dt.Format("01-02-2006 15:04:05"))
-		token2 := c.Publish("testtopic/james/curtime", 1, false, curr)
-		token2.Wait()
 	}
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(2500 * time.Second)
 
 	//unsubscribe from /go-mqtt/sample
-	if token := c.Unsubscribe("testtopic/james/"); token.Wait() && token.Error() != nil {
+	if token := c.Unsubscribe("device/james/"); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
